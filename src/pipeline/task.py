a98db09bdb315ae16ba0958299d76e08570e9fa0
nielsvaneldik <nveldik@hotmail.com>
2021-10-22 08:14:46 +0200

    more stuff

--- src/pipeline/task.py
+++ src/pipeline/task.py
@@ -1,4 +1,18 @@
+from pipeline import locator
+import functools
+
+
+def _create_function(description):
+    function = locator.get(description["name"])
+
+    default_args = description.get("default_args", None)
+    if default_args is not None:
+        return functools.partial(function, **default_args)
+    else:
+        return function
 
 
 def create_task(description):
-    return None
+    function = _create_function(description["function"])
+
+    return function
--- tests/pipeline/test_task.py
+++ tests/pipeline/test_task.py
@@ -1,17 +1,17 @@
 from datetime import datetime
+from pipeline.task import create_task
 
 
 def test_calls_correct_function():
     task_template = {
         "name": "convert_date",
         "function": {
-            "name": "datetime.datetime.strptime",
-            "default_args": {"format": "%Y-%m-%d"}
+            "name": "datetime.datetime.fromisoformat",
         },
         "input": [{"name": "date_str"}],
         "output": [{"name": "input_date"}]
     }
-    #
-    # task = create_task(task_template)
-    #
-    # assert task("2021-02-01") == datetime(year=2021, month=2, day=1)
+
+    task = create_task(task_template)
+
+    assert task("2021-02-01") == datetime(year=2021, month=2, day=1)
